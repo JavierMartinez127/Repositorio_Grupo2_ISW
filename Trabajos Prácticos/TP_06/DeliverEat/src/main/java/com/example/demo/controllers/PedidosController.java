e5d125f707e65ebee01a0e8178f642d95b00b0c0
JavierMartinez127 <103858938+JavierMartinez127@users.noreply.github.com>
2022-04-23 11:21:32 -0300

    Incio de aplicación
    Se crea la aplicación. Se crea el controlador de ejemplo y una vista de ejemplo

---
+++ Trabajos Prácticos/TP_06/DeliverEat/src/main/java/com/example/demo/controllers/PedidosController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("pedidos")
+@RequiredArgsConstructor
+public class PedidosController {
+    
+    @GetMapping("index")
+    public String GetPedido(){
+        return "pedidos/index";
+    }
+}
