50ea871cc62fca56dcf4fd72d23fdedf4bfbdc47
daniel648 <daniel@reweb.com.ar>
2022-04-24 00:32:48 -0300

    crea tarjeta

--- Trabajos Prácticos/TP_06/DeliverEat/src/main/java/com/example/demo/models/Tarjeta.java
+++ Trabajos Prácticos/TP_06/DeliverEat/src/main/java/com/example/demo/models/Tarjeta.java
@@ -1,22 +1,48 @@
 package com.example.demo.models;
 
-import java.time.LocalDate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /* Si paga con Tarjeta VISA debe ingresar el número de la tarjeta, nombre y apellido del Titular, fecha de
 vencimiento (MM/AAAA) y CVC. */
 
 @NoArgsConstructor
+@Data
 public class Tarjeta {
 
-  private Long numeroDeTargeta;
+  @NotBlank
+  @NotNull
+  @Pattern(regexp = "^(?:4[0-9]{12}(?:[0-9]{3})?)$", message = "Numero Invalido o no corresponde a una tarjeta VISA")
+  private String numero;
 
+  @NotBlank
+  @NotNull
+  @Size(max = 16)
   private String nombreTitular;
 
+  @NotBlank
+  @NotNull
+  @Size(max = 16)
   private String apellidoTitular;
 
-  private LocalDate fechaVencimiento;
+  @NotBlank
+  @NotNull
+  @Pattern(regexp = "^[0-9]{2}$",  message = "Numero Invalido")
+  private String mesVencimiento;
 
-  
+  @NotBlank
+  @NotNull
+  @Pattern(regexp = "^[0-9]{2}$", message = "Numero Invalido")
+  private String anioVencimiento;
+
+  @NotBlank
+  @NotNull
+  @Pattern(regexp = "^[0-9]{3}$", message = "Numero Invalido")
+  private String  codigoSeguridad;
+
 }
